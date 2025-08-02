3d373e11a9b5a5eb76493027046410d423b87302
Alejandro Baran <baran.alejandro04@gmail.com>
2025-08-02 00:20:45 -0300

    seccion chats y notificaciones terminada

---
+++ src/Components/SettingsContainer/NotificationsContainer/NotificationsContainer.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from 'react'
+import './NotificationsContainer.css'
+
+const NotificationsContainer = ({notifications, setNotifications}) => {
+    const [active, setActive] = useState(false)
+    const [active2, setActive2] = useState(false)
+    const [active3, setActive3] = useState(false)
+    const [active4, setActive4] = useState(false)
+    const [active5, setActive5] = useState(false)
+    const [active6, setActive6] = useState(false)
+    
+    return (
+        <>
+            <header className='new-community-header'>
+                <button className='new-community-back-button' onClick={() => setNotifications(false)}>
+                    <svg viewBox="0 0 24 24" height="24" width="24" preserveAspectRatio="xMidYMid meet" version="1.1" x="0px" y="0px" enableBackground="new 0 0 24 24"><path fill="#aebbc2" d="M12,4l1.4,1.4L7.8,11H20v2H7.8l5.6,5.6L12,20l-8-8L12,4z"></path></svg>
+                </button>
+                <h3 className='account-title'>Notificaciones</h3>
+            </header>
+            <div className='privacy-body'>
+                <div className='privacy-body-separator'>
+                    <span className='text-green'>Mensajes</span>
+                    <div className='container-with-green-switch'>
+                        <p className='privacy-list-item-p'>
+                            Notificaciones de mensajes
+                            <span className='privacy-list-item-span'>
+                                Se muestran notificaciones de mensajes nuevos.
+                            </span>
+                        </p>
+                        <div className={active ? "green-switch-active" : "green-switch"} onClick={() => setActive(!active)}>
+                            <div className={active ? "green-switch-circle-active" : "green-switch-circle"}></div>
+                        </div>
+                    </div>
+                    <div className='container-with-green-switch'>
+                        <p className='privacy-list-item-p'>
+                            Mostrar vista previa
+                        </p>
+                        <div className={active2 ? "green-switch-active" : "green-switch"} onClick={() => setActive2(!active2)}>
+                            <div className={active2 ? "green-switch-circle-active" : "green-switch-circle"}></div>
+                        </div>
+                    </div>
+                    <div className='container-with-green-switch'>
+                        <p className='privacy-list-item-p'>
+                            Mostrar notificaciones de reacciones
+                        </p>
+                        <div className={active3 ? "green-switch-active" : "green-switch"} onClick={() => setActive3(!active3)}>
+                            <div className={active3 ? "green-switch-circle-active" : "green-switch-circle"}></div>
+                        </div>
+                    </div>
+                    <div className='container-with-green-switch'>
+                        <p className='privacy-list-item-p'>
+                            Sincronización en segundo plano
+                            <span className='privacy-list-item-span'>
+                                Sincroniza los mensajes en segundo plano para obtener un rendimiento más rápido.
+                            </span>
+                        </p>
+                        <div className={active4 ? "green-switch-active" : "green-switch"} onClick={() => setActive4(!active4)}>
+                            <div className={active4 ? "green-switch-circle-active" : "green-switch-circle"}></div>
+                        </div>
+                    </div>
+                    <div className='container-with-green-switch'>
+                        <p className='privacy-list-item-p'>
+                            Sonidos para mensajes entrantes
+                            <span className='privacy-list-item-span'>
+                                Se reproduce un sonido cuando hay mensajes entrantes.
+                            </span>
+                        </p>
+                        <div className={active5 ? "green-switch-active" : "green-switch"} onClick={() => setActive5(!active5)}>
+                            <div className={active5 ? "green-switch-circle-active" : "green-switch-circle"}></div>
+                        </div>
+                    </div>
+                    <div className='container-with-green-switch'>
+                        <p className='privacy-list-item-p'>
+                            Sonidos para mensajes salientes
+                            <span className='privacy-list-item-span'>
+                                Se reproduce un sonido cuando hay mensajes salientes.
+                            </span>
+                        </p>
+                        <div className={active5 ? "green-switch-active" : "green-switch"} onClick={() => setActive6(!active5)}>
+                            <div className={active5 ? "green-switch-circle-active" : "green-switch-circle"}></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default NotificationsContainer
--- src/Components/SettingsContainer/SettingsContainer.jsx
+++ src/Components/SettingsContainer/SettingsContainer.jsx
@@ -3,31 +3,35 @@ import './SettingsContainer.css'
 import { useEditing } from '../../Context/EditingContext'
 import AccountContainer from './AccountContainer/AccountContainer'
 import PrivacyContainer from './PrivacyContainer/PrivacyContainer'
+import ChatsContainer from './ChatsContainer/ChatsContainer'
+import NotificationsContainer from './NotificationsContainer/NotificationsContainer'
 
 const SettingsContainer = () => {
     const { name, info } = useEditing()
     const [account, setAccount] = useState(false)
     const [privacy, setPrivacy] = useState(false)
+    const [chats, setChats] = useState(false)
+    const [notifications, setNotifications] = useState(false)
     return (
         <div className='status-container'>
-            <header className='channels-header'  style={{display: privacy || account ? "none" : "flex"}}>
+            <header className='channels-header'  style={{display: chats || privacy || account || notifications ? "none" : "flex"}}>
                 <h1 className='chats-list-header-title'>Ajustes</h1>
             </header>
-            <form className='settings-form'  style={{display: privacy || account ? "none" : "flex"}}>
+            <form className='settings-form'  style={{display: chats || privacy || account || notifications ? "none" : "flex"}}>
                 <label>Search</label>
                 <button type='submit' className='chats-list-search-button'>
                     <svg viewBox="0 0 24 24" height="24" width="24" preserveAspectRatio="xMidYMid meet" version="1.1" x="0px" y="0px" ><title>search</title><path fill="#8797a1" d="M15.009,13.805h-0.636l-0.22-0.219c0.781-0.911,1.256-2.092,1.256-3.386 c0-2.876-2.332-5.207-5.207-5.207c-2.876,0-5.208,2.331-5.208,5.207s2.331,5.208,5.208,5.208c1.293,0,2.474-0.474,3.385-1.255 l0.221,0.22v0.635l4.004,3.999l1.194-1.195L15.009,13.805z M10.201,13.805c-1.991,0-3.605-1.614-3.605-3.605 s1.614-3.605,3.605-3.605s3.605,1.614,3.605,3.605S12.192,13.805,10.201,13.805z"></path></svg>
                 </button>
                 <input className='settings-input' type='text' placeholder='Buscar en los ajustes' name='search' autoComplete='off'></input>
             </form>
-            <div className='settings-profile-container'  style={{display: privacy || account ? "none" : "flex"}}>
+            <div className='settings-profile-container'  style={{display: chats || privacy || account || notifications ? "none" : "flex"}}>
                 <img src="/images/pepe-profile.jpg" alt='pepe-profile' className='settings-profile'/>
                 <div className='settings-info-container'>
                     <p className='settings-username'>{name}</p>
                     <span className='settings-status'>{info}</span>
                 </div>
             </div>
-            <div className='settings-list-container'  style={{display: privacy || account ? "none" : "block"}}>
+            <div className='settings-list-container'  style={{display: chats || privacy || account || notifications ? "none" : "block"}}>
                 <ul>
                     <li className='settings-list' onClick={() => setAccount(true)}>
                         <svg viewBox="0 0 24 24" height="24" width="24" preserveAspectRatio="xMidYMid meet" fill="none"><path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 5C13.66 5 15 6.34 15 8C15 9.66 13.66 11 12 11C10.34 11 9 9.66 9 8C9 6.34 10.34 5 12 5ZM12 19.2C9.5 19.2 7.29 17.92 6 15.98C6.03 13.99 10 12.9 12 12.9C13.99 12.9 17.97 13.99 18 15.98C16.71 17.92 14.5 19.2 12 19.2Z" fill="#d0d7db"></path></svg>
@@ -37,11 +41,11 @@ const SettingsContainer = () => {
                         <svg viewBox="0 0 28 35" height="23" width="23" preserveAspectRatio="xMidYMid meet"  version="1.1"><path d="M14,1.10204082 C18.5689011,1.10204082 22.2727273,4.80586698 22.2727273,9.37476809 L22.272,12.1790408 L22.3564837,12.181606 C24.9401306,12.294858 27,14.4253101 27,17.0368705 L27,29.4665309 C27,32.1506346 24.824104,34.3265306 22.1400003,34.3265306 L5.85999974,34.3265306 C3.175896,34.3265306 1,32.1506346 1,29.4665309 L1,17.0368705 C1,14.3970988 3.10461313,12.2488858 5.72742704,12.178644 L5.72727273,9.37476809 C5.72727273,4.80586698 9.43109889,1.10204082 14,1.10204082 Z M14,19.5600907 C12.0418995,19.5600907 10.4545455,21.2128808 10.4545455,23.2517007 C10.4545455,25.2905206 12.0418995,26.9433107 14,26.9433107 C15.9581005,26.9433107 17.5454545,25.2905206 17.5454545,23.2517007 C17.5454545,21.2128808 15.9581005,19.5600907 14,19.5600907 Z M14,4.79365079 C11.4617216,4.79365079 9.39069048,6.79417418 9.27759175,9.30453585 L9.27272727,9.52092352 L9.272,12.1760408 L18.727,12.1760408 L18.7272727,9.52092352 C18.7272727,6.91012289 16.6108006,4.79365079 14,4.79365079 Z" fill="#d0d7db"></path></svg>
                         <p className='settings-list-text'>Privacidad</p>
                     </li>
-                    <li className='settings-list'>
+                    <li className='settings-list' onClick={() => setChats(true)}>
                         <svg viewBox="0 0 24 24" height="24" width="24" preserveAspectRatio="xMidYMid meet" fill="none"><path fillRule="evenodd" clipRule="evenodd" d="M4.8384 8.45501L5 8.70356V9V17.8333C5 18.7538 5.7462 19.5 6.6667 19.5H20.3333C21.2538 19.5 22 18.7538 22 17.8333V6.66667C22 5.74619 21.2538 5 20.3333 5H2.5927L4.8384 8.45501ZM8 14.5C8 13.6716 8.67157 13 9.5 13H14.5C15.3284 13 16 13.6716 16 14.5C16 15.3284 15.3284 16 14.5 16H9.5C8.67157 16 8 15.3284 8 14.5ZM9.5 8C8.67157 8 8 8.67157 8 9.5C8 10.3284 8.67157 11 9.5 11H16.5C17.3284 11 18 10.3284 18 9.5C18 8.67157 17.3284 8 16.5 8H9.5Z" fill="#d0d7db"></path><path d="M5 8.70356L5.41919 8.43101L5.5 8.55531V8.70356H5ZM4.8384 8.45501L4.41921 8.72756L4.41917 8.7275L4.8384 8.45501ZM2.5927 5L2.17347 5.27249L1.67137 4.5H2.5927V5ZM4.58081 8.9761L4.41921 8.72756L5.25759 8.18247L5.41919 8.43101L4.58081 8.9761ZM4.5 9V8.70356H5.5V9H4.5ZM4.5 17.8333V9H5.5V17.8333H4.5ZM6.6667 20C5.47006 20 4.5 19.0299 4.5 17.8333H5.5C5.5 18.4777 6.02234 19 6.6667 19V20ZM20.3333 20H6.6667V19H20.3333V20ZM22.5 17.8333C22.5 19.0299 21.53 20 20.3333 20V19C20.9777 19 21.5 18.4777 21.5 17.8333H22.5ZM22.5 6.66667V17.8333H21.5V6.66667H22.5ZM20.3333 4.5C21.53 4.5 22.5 5.47005 22.5 6.66667H21.5C21.5 6.02233 20.9777 5.5 20.3333 5.5V4.5ZM2.5927 4.5H20.3333V5.5H2.5927V4.5ZM4.41917 8.7275L2.17347 5.27249L3.01192 4.72751L5.25762 8.18252L4.41917 8.7275ZM9.5 13.5C8.94772 13.5 8.5 13.9477 8.5 14.5H7.5C7.5 13.3954 8.39543 12.5 9.5 12.5V13.5ZM14.5 13.5H9.5V12.5H14.5V13.5ZM15.5 14.5C15.5 13.9477 15.0523 13.5 14.5 13.5V12.5C15.6046 12.5 16.5 13.3954 16.5 14.5H15.5ZM14.5 15.5C15.0523 15.5 15.5 15.0523 15.5 14.5H16.5C16.5 15.6046 15.6046 16.5 14.5 16.5V15.5ZM9.5 15.5H14.5V16.5H9.5V15.5ZM8.5 14.5C8.5 15.0523 8.94772 15.5 9.5 15.5V16.5C8.39543 16.5 7.5 15.6046 7.5 14.5H8.5ZM7.5 9.5C7.5 8.39543 8.39543 7.5 9.5 7.5V8.5C8.94772 8.5 8.5 8.94772 8.5 9.5H7.5ZM9.5 11.5C8.39543 11.5 7.5 10.6046 7.5 9.5H8.5C8.5 10.0523 8.94772 10.5 9.5 10.5V11.5ZM16.5 11.5H9.5V10.5H16.5V11.5ZM18.5 9.5C18.5 10.6046 17.6046 11.5 16.5 11.5V10.5C17.0523 10.5 17.5 10.0523 17.5 9.5H18.5ZM16.5 7.5C17.6046 7.5 18.5 8.39543 18.5 9.5H17.5C17.5 8.94772 17.0523 8.5 16.5 8.5V7.5ZM9.5 7.5H16.5V8.5H9.5V7.5Z" fill="#d0d7db"></path></svg>
                         <p className='settings-list-text'>Chats</p>
                     </li>
-                    <li className='settings-list'>
+                    <li className='settings-list' onClick={() => setNotifications(true)}>
                         <svg viewBox="0 0 24 24" height="27" width="27" preserveAspectRatio="xMidYMid meet" version="1.1" x="0px" y="0px"><path fill="#d0d7db" d="M12,21.7c0.9,0,1.7-0.8,1.7-1.7h-3.4C10.3,20.9,11.1,21.7,12,21.7z M17.6,16.5v-4.7 c0-2.7-1.8-4.8-4.3-5.4V5.8c0-0.7-0.6-1.3-1.3-1.3s-1.3,0.6-1.3,1.3v0.6C8.2,7,6.4,9.1,6.4,11.8v4.7l-1.7,1.7v0.9h14.6v-0.9 L17.6,16.5z"></path></svg>
                         <p className='settings-list-text'>Notificaciones</p>
                     </li>
@@ -61,6 +65,8 @@ const SettingsContainer = () => {
             </div>
             {account && <AccountContainer setAccount={setAccount} account={account}/>}
             {privacy && <PrivacyContainer setPrivacy={setPrivacy} privacy={privacy} />}
+            {chats && <ChatsContainer setChats={setChats} chats={chats}/>}
+            {notifications && <NotificationsContainer setNotifications={setNotifications} notifications={notifications}/>}
         </div>
     )
 }
